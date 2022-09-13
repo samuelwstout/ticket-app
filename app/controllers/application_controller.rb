ccc4eaf16b88879d11fb7a4efde6b03df0721afa
samuelwstout <samuelwstout@gmail.com>
2022-09-13 12:28:13 -0500

    add session actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  before_action :confirm_authentication
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def confirm_authentication
+    render json: { error: "You must be logged in to do that." }, status: :unauthorized unless logged_in?
+  end
+
+  def not_found(e)
+    render json: { error: e.message }, status: :not_found
+  end
 
 end
