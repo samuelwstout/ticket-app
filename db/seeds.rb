9b16a7d2cc4199255f60f07ce5f0840e165793ab
samuelwstout <samuelwstout@gmail.com>
2022-09-13 23:45:59 -0500

    install hirb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'date'
+# require 'date'
 
-User.create(username: 'username1', password: 'password')
-User.create(username: 'username2', password: 'password')
-User.create(username: 'username3', password: 'password')
+# User.create(username: 'username1', password: 'password')
+# User.create(username: 'username2', password: 'password')
+# User.create(username: 'username3', password: 'password')
 
-Concert.create(title: 'concert1', date: Date.new(2022,9,13), description: 'description for concert1')
-Concert.create(title: 'concert2', date: Date.new(2022,9,14), description: 'description for concert2')
-Concert.create(title: 'concert3', date: Date.new(2022,9,15), description: 'description for concert3')
+# Concert.create(title: 'concert1', date: Date.new(2022,9,13), description: 'description for concert1')
+# Concert.create(title: 'concert2', date: Date.new(2022,9,14), description: 'description for concert2')
+# Concert.create(title: 'concert3', date: Date.new(2022,9,15), description: 'description for concert3')
 
-Ticket.create(price: 100, user_id: 1, concert_id: 1)
-Ticket.create(price: 100, user_id: 1, concert_id: 2)
-Ticket.create(price: 110, user_id: 3, concert_id: 1)
+# Ticket.create(price: 100, user_id: 1, concert_id: 1)
+# Ticket.create(price: 100, user_id: 1, concert_id: 2)
+# Ticket.create(price: 110, user_id: 3, concert_id: 1)
