d4c68afa0cce328fbb1f4e908549156e367ae0d2
samuelwstout <samuelwstout@gmail.com>
2022-09-18 15:32:10 -0500

    clear myTickets

--- client/src/components/App/MyTickets.jsx
+++ client/src/components/App/MyTickets.jsx
@@ -1,90 +1,13 @@
-import {useState, useEffect} from 'react'
 import Navigation from "../Navigation"
 
-const MyTickets = ({setUser, user, userId}) => {
+const MyTickets = ({setUser, user}) => {
  
-  const [tickets, setTickets] = useState([])
-  const [deleteId, setDeleteId] = useState()
-  const [editText, setEditText] = useState('')
-  const [editId, setEditId] = useState()
-
-  // Read all tickets
-  useEffect(() => {
-    if (userId) {
-    fetch('/api/tickets')
-    .then(res => res.json())
-    .then(res => setTickets(res.filter(ticket => ticket.user_id === userId)))
-    }
-  }, [userId, setTickets])
-
-  // Delete ticket
-  useEffect(() => {
-    if (deleteId !== undefined) {
-      fetch(`/api/tickets/${deleteId}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-      .then(r => r.json())
-      .then(data => {
-        // establishes data.id and ticket.id
-        const ticketIds = tickets.map(t => t.id)
-        const index = ticketIds.indexOf(data.id)
-        ticketIds.splice(index, 1)
-        const finalArray = tickets.filter(t => t.id !== data.id)
-        setTickets(finalArray.map(a => a))
-      })
-    }
-  }, [])
-
-// // Update ticket
-//   useEffect(() => {
-//   if (editId !== undefined) {
-//     fetch(`/api/tickets/${editId}`, {
-//       method: 'PATCH',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         user_notes: editText
-//       })
-//     })
-//     .then(r => r.json())
-//     .then(data => {
-//       const old = tickets.find(x => x.id === data.id)
-//       const array = tickets.map(t => t)
-//       array.splice(array.findIndex(s => s === old), 1)
-//       array.push(data)
-//       const updateData = array.map(a => a)
-//       setTickets(updateData)
-//     })
-//   }
-//   }, [editId, editText, tickets])
 
 
   return (
     <div>
       <Navigation setUser={setUser} user={user} />
-      {tickets.map(ticket => {
-        return (
-          <div key={ticket.id}>
-            <h3>Ticket: {ticket.title}</h3>
-            <ul>
-              <li>Ticket number: {ticket.id}</li>
-              <li>{ticket.date}</li>
-              <li>{ticket.description}</li>
-              <li>${ticket.price}</li>
-              <li>{ticket.user_notes}</li> 
-              <li><form onSubmit={(e) => {
-                e.preventDefault()
-                setEditId(ticket.id)
-              }}><label>To edit user notes: </label><input type='text' onChange={(e) => setEditText(e.target.value)} /> <input type='submit' /></form></li>
-              <li><button onClick={() => setDeleteId(ticket.id)}>Delete</button></li>
-            </ul>
-          </div>
-        )
-      })}
+      <h1>Tickets here</h1>
     </div>
   )
 }
