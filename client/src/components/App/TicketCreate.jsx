f79e91178280fd3d4972f39efdc5d979cd84b93b
samuelwstout <samuelwstout@gmail.com>
2022-09-23 21:35:28 -0500

    create ticket mui build

--- client/src/components/App/TicketCreate.jsx
+++ client/src/components/App/TicketCreate.jsx
@@ -1,7 +1,14 @@
 import {useState} from 'react'
 import Navigation from '../Navigation'
 import {useParams} from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
+const theme = createTheme();
 
 const TicketCreate = ({userId, concerts, user, setUser, setTickets, tickets }) => {
 
@@ -45,24 +52,77 @@ const handleSubmit = (e) => {
   return (
     <div>
         <Navigation setUser={setUser} user={user} />
-        {concert &&
-        <div>
-        <h1>Buy a ticket to {concert.title}</h1>
-        <h2>Date: {concert.date}</h2>
-        <h2>Description: {concert.description}</h2>
-        <h2>${concert.price}</h2>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='user_notes'>You can leave a note if you'd like: </label>
-            <input type='text' name='user_notes' value={userNotes} onChange={(e) => setUserNotes(e.target.value)} />
-            <input type='submit' />
-        </form>
-        </div>
-        }
-        {ticketId &&
-        <div>
-            <h1>You bought ticket #{ticketId}</h1>
-        </div>
-        }
+        <ThemeProvider theme={theme}>
+            <main>
+            <Box
+          sx={{
+            bgcolor: 'background.paper',
+            marginTop: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+            >
+            {concert &&
+            <Container maxWidth="m">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Buy a ticket to {concert.title}
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary">
+              {concert.date}
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary">
+              {concert.description}
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary">
+              ${concert.price} per ticket
+            </Typography>
+          </Container>
+            } 
+             <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Typography variant="h5" align="center" color="text.secondary">
+                Leave a note:
+            </Typography>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="note"
+              label="Note"
+              name="note"
+              autoComplete="note"
+              autoFocus
+              value={userNotes}
+              onChange={(e) => setUserNotes(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+            </Box> 
+            </Box>
+            {ticketId && 
+                <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                You bought ticket #{ticketId}
+              </Typography>
+            }
+            </main>
+        </ThemeProvider>
     </div>
   )
 }
